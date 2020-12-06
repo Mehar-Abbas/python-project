3012af8daee29008861cb7bc85f84c698d76ca60
Mehar Abbas <mabbas@my.waketech.edu>
2020-12-05 23:02:07 -0500

    redirect-to-app

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 import tkinter as tk
 
 app = Flask(__name__)
 
 @app.route('/')
+def go_to_app():
+    return redirect(url_for('http://tic-tac-toe-tic-tac-toe.192.168.99.103.nip.io'))
 
-def print_hello():
-    # Use a breakpoint in the code line below to debug your script.
-    return "Hello from the app"  # Press Ctrl+F8 to toggle the breakpoint.
+#
+# def print_hello():
+#     # Use a breakpoint in the code line below to debug your script.
+#     return "Hello from the app"  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hello()
+    # print_hello()
     app.run()
