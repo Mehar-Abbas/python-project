ee42dfd61d7fa9ad97b89d813b184d59b5b9e9ea
Mehar Abbas <mabbas@my.waketech.edu>
2020-12-11 15:38:26 -0500

    created game-window

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import os
 from flask import Flask, render_template
-# import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+# the logic of Tic Tac Toe ------------------------------------------------
+
+game_window = Tk()
+game_window.title("TIC - TAC - TOE")
+
+
+game_window.mainloop()
+
+
+
+
+
 
 app = Flask(__name__)
 
@@ -13,4 +27,4 @@ def print_hello():
 
 if __name__ == "__main__":
     # print_hello()
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
